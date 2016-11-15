cfe79aa81144f8a4008b57a83d881b5d988ba503
kura5 <laboradian@gmail.com>
2016-11-15 13:51:54 +0900

    Add "type" to a script tag

--- index.php
+++ index.php
@@ -292,7 +292,7 @@ body {
     crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   <script src="https://use.fontawesome.com/c215ece6c6.js"></script>
-  <script nonce='<?php echo e($nonce1); ?>'>
+  <script type="text/javascript" nonce='<?php echo e($nonce1); ?>'>
 $(function(){
   $('#ossize').text(screen.width + ' x ' + screen.height + ' pixel');
 });
