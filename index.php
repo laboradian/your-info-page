c262729858d8676b0c58aa241926834e5d0a5cfa
kura5 <laboradian@gmail.com>
2016-11-28 15:18:59 +0900

    Remove non-needed 2 spaces

--- index.php
+++ index.php
@@ -122,7 +122,7 @@ header("X-Frame-Options: DENY");
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge"  >
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>あなたのウェブブラウザが送信する情報</title>
   <!-- Latest compiled and minified CSS -->
